13da62432398573698ec8c9e8a43dce2b5585df0
Simão Neto <simaomenezesneto@gmail.com>
2022-07-22 20:46:19 +0100

    feat: Creat instance of categoriesRepository

--- RentAPI/src/modules/cars/useCases/listCategories/index.ts
+++ RentAPI/src/modules/cars/useCases/listCategories/index.ts
@@ -2,7 +2,7 @@ import { CategoriesRepository } from "../../repositories/CategoriesRepository";
 import { ListCategoriesController } from "./ListCategoriesController";
 import { ListCategoriesUserCase } from "./ListCategoriesUseCase";
 
-const categoriesRepository = new CategoriesRepository();
+const categoriesRepository = CategoriesRepository.getInstance();
 const listCategoriesUseCase = new ListCategoriesUserCase(categoriesRepository);
 const listCategoriesController = new ListCategoriesController(listCategoriesUseCase);
 
